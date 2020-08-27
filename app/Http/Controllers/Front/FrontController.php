a44c143bc260ae6752166559d8b9a779fed31124
tuhsarwdev <tusharwdev@gmail.com>
2020-08-27 13:01:02 +0600

    featured product shown front

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers\Front;
 
+use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Controller;
+use App\product;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     public function home (){
-        return view('front.home');
+
+        $data['featured_products'] = product::where('is_featured',1)->limit(8)->get();
+        return view('front.home',$data);
     }
 }
